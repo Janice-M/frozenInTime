416a9b4d9f152a9fd65715c221a74017e52ffe92
Janice-M <janiceink001@gmail.com>
2019-08-25 17:33:09 +0300

    worked on photos/admin.py

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Image,Category,Location
 
-# Register your models here.
+# Register your models here habibty bruh bruh
+
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
+
