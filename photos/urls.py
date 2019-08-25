73e8bb7c63862ff885c6588012dc2c4641b58807
Janice-M <janiceink001@gmail.com>
2019-08-25 16:23:07 +0300

    created rootsfrom views to url  in photos django app

---
+++ photos/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^$', views.welcome, name='welcome'),
+    url(r'^search/',views.search_image,name='search_image'),
+    url(r'^category/',views.category),
+    url(r'^location/(?P<loc>\w+)/', views.get_image_by_location, name='location'),
+    url(r'^image/(?P<image_id>\d+)', views.single_image, name='image'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
