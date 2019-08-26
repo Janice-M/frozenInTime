61516ec1a5cd7009824ee04b8c49fe874d7e2daa
Janice-M <janiceink001@gmail.com>
2019-08-26 11:25:47 +0300

    deployment to heroku guide

--- frozenInTime/settings.py
+++ frozenInTime/settings.py
@@ -147,3 +147,8 @@ STATICFILES_DIRS = (
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
